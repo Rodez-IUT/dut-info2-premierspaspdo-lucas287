4bf3b6be0c29ffeac16c3bf97be4fb82dd64f2b2
lucas287 <lucaslegamer287@gmail.com>
2019-11-06 10:30:16 +0100

    à continuer

---
+++ all_users.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8" />
+	<title>Bonjour</title>
+	<style type="text/css">
+		table{
+			border-style: solid;
+			border-width: 1px;
+		}
+	</style>
+</head>
+<body>
+	<table>
+		<tr>
+			<th>Id</th>
+			<th>Username</th>
+			<th>Email</th>
+			<th>Status</th>
+		</tr>
+	</table>
+</body>
+</html>

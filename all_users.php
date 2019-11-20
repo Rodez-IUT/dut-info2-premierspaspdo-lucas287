ee2b6c4e512ae0d10eefb5c65ad38f9b39a72d65
lucas287 <lucaslegamer287@gmail.com>
2019-11-20 10:00:26 +0100

    fix indentation lol

--- all_users.php
+++ all_users.php
@@ -18,13 +18,13 @@
 <body>
 	<form method="post">
 		<select name="actif">
-				<option>Active account</option>
-				<option>Waiting for account validation</option>
-			</select>
-			
-			<input type="text" name="nom" />
+			<option>Active account</option>
+			<option>Waiting for account validation</option>
+		</select>
+		
+		<input type="text" name="nom" />
 
-			<input type="submit" />
+		<input type="submit" />
 	</form>
 		<table> 
 			<tr class="entete"> 
@@ -33,45 +33,45 @@
 				<th> Email </th> 
 				<th> Status </th> 
 			</tr>
-	<?php
-		if (isset($_POST['actif']) && isset($_POST['nom'])) {
-		/* initialisation des différentes variables du PDO */
-		$host = 'localhost';
-		$db   = 'my_activities';
-		$user = 'root';
-		$pass = 'root';
-		$charset = 'utf8';
-		
-		/* définition du PDO */
-		$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-		
-		$options = [
-			PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-			PDO::ATTR_EMULATE_PREPARES   => false,
-		];
-		
-		try {
-			/* création de l'accès à la BD */
-			$pdo = new PDO($dsn, $user, $pass, $options);
-		} catch (PDOException $e) {
-			throw new PDOException($e->getMessage(), (int)$e->getCode());
-		}
-		
-		$stmt = $pdo->prepare('SELECT * FROM users AS u JOIN status AS s ON u.status_id=s.id WHERE s.name = ? AND u.username LIKE ? ORDER BY username');
-		$stmt->execute([$_POST['actif'],$_POST['nom'].'%']);	
-	
-		while ($row = $stmt->fetch())
-		{
-				echo '<tr>';
-				echo '<td>'.$row['id'].'</td>';
-				echo '<td>'.$row['username'].'</td>';
-				echo '<td>'.$row['email'].'</td>';
-				echo '<td>'.$row['name'].'</td>';
-				echo '</tr>';
-		}
-	}
-	?>
-		</table>
+		<?php
+			if (isset($_POST['actif']) && isset($_POST['nom'])) {
+			/* initialisation des différentes variables du PDO */
+				$host = 'localhost';
+				$db   = 'my_activities';
+				$user = 'root';
+				$pass = 'root';
+				$charset = 'utf8';
+				
+				/* définition du PDO */
+				$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+				
+				$options = [
+					PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+					PDO::ATTR_EMULATE_PREPARES   => false,
+				];
+				
+				try {
+					/* création de l'accès à la BD */
+					$pdo = new PDO($dsn, $user, $pass, $options);
+				} catch (PDOException $e) {
+					throw new PDOException($e->getMessage(), (int)$e->getCode());
+				}
+				
+				$stmt = $pdo->prepare('SELECT * FROM users AS u JOIN status AS s ON u.status_id=s.id WHERE s.name = ? AND u.username LIKE ? ORDER BY username');
+				$stmt->execute([$_POST['actif'],$_POST['nom'].'%']);	
+			
+				while ($row = $stmt->fetch())
+				{
+						echo '<tr>';
+						echo '<td>'.$row['id'].'</td>';
+						echo '<td>'.$row['username'].'</td>';
+						echo '<td>'.$row['email'].'</td>';
+						echo '<td>'.$row['name'].'</td>';
+						echo '</tr>';
+				}
+			}
+		?>
+	</table>
 </body>
 </html>
